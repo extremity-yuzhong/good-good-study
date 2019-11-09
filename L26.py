18895e6af82072a81f779d6ae8d02fa62a17d185
yyl447522442 <42394913+yyl447522442@users.noreply.github.com>
2019-11-09 09:12:57 +0800

    删除排序数组中的重复项

---
+++ L26.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not len(nums): return 0
+        length = len(nums)
+        r = 1
+        s = 0
+        for i in range(1, length):
+            if nums[i] != nums[s]:
+                r += 1
+                s += 1
+                nums[s] = nums[i]
+        return r
+
+
+nums = [1, 1, 1, 1, 1, 2, 2, 3]
+nums = [1, 1, 2]
+nums = []
+print Solution().removeDuplicates(nums)
+print nums
